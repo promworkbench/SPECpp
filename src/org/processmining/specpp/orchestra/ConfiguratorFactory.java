e195d0a5cee49f5d0ea1357d5721adaf9b9edc04
leah-tgu <leah.tgu@gmail.com>
2022-09-23 10:38:56 +0000

    version control dual wielding
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/SPECpp/Trunk@46707 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/specpp/orchestra/ConfiguratorFactory.java
@@ -0,0 +1,102 @@
+package org.processmining.specpp.orchestra;
+
+import org.processmining.specpp.base.AdvancedComposition;
+import org.processmining.specpp.base.impls.PlaceComposerWithCIPR;
+import org.processmining.specpp.base.impls.PlaceFitnessFilter;
+import org.processmining.specpp.componenting.data.ParameterRequirements;
+import org.processmining.specpp.componenting.evaluation.EvaluatorConfiguration;
+import org.processmining.specpp.componenting.system.GlobalComponentRepository;
+import org.processmining.specpp.composition.ConstrainingPlaceCollection;
+import org.processmining.specpp.composition.TrackingPlaceCollection;
+import org.processmining.specpp.config.*;
+import org.processmining.specpp.config.parameters.ParameterProvider;
+import org.processmining.specpp.config.parameters.SupervisionParameters;
+import org.processmining.specpp.datastructures.petri.PetriNet;
+import org.processmining.specpp.datastructures.petri.Place;
+import org.processmining.specpp.datastructures.petri.ProMPetrinetWrapper;
+import org.processmining.specpp.datastructures.tree.base.impls.EnumeratingTree;
+import org.processmining.specpp.datastructures.tree.heuristic.HeuristicUtils;
+import org.processmining.specpp.datastructures.tree.heuristic.TreeNodeScore;
+import org.processmining.specpp.datastructures.tree.nodegen.MonotonousPlaceGenerationLogic;
+import org.processmining.specpp.datastructures.tree.nodegen.PlaceNode;
+import org.processmining.specpp.datastructures.tree.nodegen.PlaceState;
+import org.processmining.specpp.evaluation.fitness.AbsolutelyNoFrillsFitnessEvaluator;
+import org.processmining.specpp.evaluation.heuristics.DirectlyFollowsHeuristic;
+import org.processmining.specpp.evaluation.heuristics.SigmoidDelta;
+import org.processmining.specpp.evaluation.markings.LogHistoryMaker;
+import org.processmining.specpp.postprocessing.LPBasedImplicitnessPostProcessing;
+import org.processmining.specpp.postprocessing.ProMConverter;
+import org.processmining.specpp.postprocessing.ReplayBasedImplicitnessPostProcessing;
+import org.processmining.specpp.prom.mvc.config.ConfiguratorCollection;
+import org.processmining.specpp.proposal.ConstrainablePlaceProposer;
+import org.processmining.specpp.supervision.supervisors.BaseSupervisor;
+import org.processmining.specpp.supervision.supervisors.EventCountsSupervisor;
+import org.processmining.specpp.supervision.supervisors.PerformanceSupervisor;
+import org.processmining.specpp.supervision.supervisors.TerminalSupervisor;
+
+public class ConfiguratorFactory {
+
+    public static void main() {
+
+        // ** Supervision ** //
+
+        SupervisionConfiguration.Configurator svConfig = Configurators.supervisors().addSupervisor(BaseSupervisor::new);
+        svConfig.addSupervisor(PerformanceSupervisor::new).addSupervisor(EventCountsSupervisor::new);
+        // detailed heuristics logger
+        // svConfig.addSupervisor(DetailedHeuristicsSupervisor::new);
+        svConfig.addSupervisor(TerminalSupervisor::new);
+
+        // ** Evaluation ** //
+
+        EvaluatorConfiguration.Configurator evConfig = Configurators.evaluators()
+                                                                    .addEvaluatorProvider(new AbsolutelyNoFrillsFitnessEvaluator.Builder())
+                                                                    .addEvaluatorProvider(new LogHistoryMaker.Builder());
+        // delta adaptation function
+        evConfig.addEvaluatorProvider(new SigmoidDelta.Builder());
+        // make heuristics available
+        evConfig.addEvaluatorProvider(new DirectlyFollowsHeuristic.Builder());
+
+        HeuristicTreeConfiguration.Configurator<Place, PlaceState, PlaceNode, TreeNodeScore> htConfig = Configurators.<Place, PlaceState, PlaceNode, TreeNodeScore>heuristicTree()
+                                                                                                                     .childGenerationLogic(new MonotonousPlaceGenerationLogic.Builder())
+                                                                                                                     .tree(EnumeratingTree::new);
+        // tree node heuristic
+        htConfig.heuristic(HeuristicUtils::bfs);
+
+        // ** Proposal & Composition ** //
+
+        ProposerComposerConfiguration.Configurator<Place, AdvancedComposition<Place>, PetriNet> pcConfig = Configurators.<Place, AdvancedComposition<Place>, PetriNet>proposerComposer()
+                                                                                                                        .nestedComposition(TrackingPlaceCollection::new, ConstrainingPlaceCollection::new)
+                                                                                                                        .proposer(new ConstrainablePlaceProposer.Builder());
+
+        pcConfig.terminalComposer(PlaceComposerWithCIPR::new);
+        // without concurrent implicit place removal
+        // pcConfig.terminalComposer(PlaceAccepter::new);
+        pcConfig.composerChain(PlaceFitnessFilter::new);
+        // pcConfig.composerChain(PlaceFitnessFilter::new, UniwiredComposer::new);
+        // pcConfig.composerChain(PlaceFitnessFilter::new, DeltaComposer::new);
+
+        // ** Post Processing ** //
+
+        PostProcessingConfiguration.Configurator<PetriNet, PetriNet> temp_ppConfig = Configurators.postProcessing();
+        // ppConfig.processor(new UniwiredSelfLoopAdditionPostProcessing.Builder());
+        // ppConfig.processor(SelfLoopPlaceMerger::new);
+        temp_ppConfig.addPostProcessor(new ReplayBasedImplicitnessPostProcessing.Builder())
+                     .addPostProcessor(new LPBasedImplicitnessPostProcessing.Builder());
+        PostProcessingConfiguration.Configurator<PetriNet, ProMPetrinetWrapper> ppConfig = temp_ppConfig.addPostProcessor(ProMConverter::new);
+
+        // ** Parameters ** //
+
+        ParameterProvider parProv = new ParameterProvider() {
+            @Override
+            public void init() {
+                globalComponentSystem().provide(ParameterRequirements.SUPERVISION_PARAMETERS.fulfilWithStatic(SupervisionParameters.instrumentAll(true, true)));
+            }
+        };
+
+
+        ConfiguratorCollection configuratorCollection = new ConfiguratorCollection(svConfig, pcConfig, evConfig, htConfig, ppConfig, parProv);
+    }
+
+
+
+}
