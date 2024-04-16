568e79042db7435a5424186634920c817d9e4553
Leah Tacke genannt Unterberg <leah.tgu@pads.rwth-aachen.de>
2024-04-16 12:06:04 +0200

    manual merge..

--- src/org/processmining/specpp/datastructures/util/SelfFillingCache.java
+++
@@ -1,7 +0,0 @@
-package org.processmining.specpp.datastructures.util;
-
-public interface SelfFillingCache<K, V> {
-
-    V get(K key);
-
-}
--- src/org/processmining/specpp/evaluation/fitness/BasicFitnessStatus.java
+++
@@ -1,7 +0,0 @@
-package org.processmining.specpp.evaluation.fitness;
-
-import org.processmining.specpp.base.CandidateEvaluation;
-
-public enum BasicFitnessStatus implements CandidateEvaluation {
-    FITTING, UNDERFED, OVERFED, ACTIVATED, UNACTIVATED
-}
